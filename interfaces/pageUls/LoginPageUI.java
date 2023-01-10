96268a8b107f30c8f679cb5ed9f020ad70f6bb20
Curry Pham <64634056+CurryPham@users.noreply.github.com>
2023-01-10 22:08:30 +0700

    init automation framework

---
+++ interfaces/pageUls/LoginPageUI.java
@@ -0,0 +1,19 @@
+package pageUls;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class LoginPageUI {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
